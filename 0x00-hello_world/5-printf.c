6b789f0dfbd972129392e0f0f05da43d9502fa98
tavershi <gogobernard41@gmail.com>
2023-09-08 07:51:10 -0700

    preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function of the main
+ * Return: 0 when sucessful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n"); return (0);
+}
