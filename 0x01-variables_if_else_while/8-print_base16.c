53a6bde39964f3b53748b18e4eafa0aa35761e44
tavershi <gogobernard41@gmail.com>
2023-09-11 11:37:10 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,11 +6,15 @@
  * Return: always 0
  */
 
-int main(void)
+int main (void)
 {
 	int i;
+	char j;
 
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
-	
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
 }
