1d5d1f26dc1343bb0ec2cae3bbe28f44160073a9
tavershi <gogobernard41@gmail.com>
2023-09-20 11:32:16 -0700

    task 9

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - function to find the absolute value of a number
+ * @a: function paramter
+ * Return: -a or a;
+ */
+
+int _abs(int)
+{
+	if (a < 0)
+		return (-a);
+	else if (a >= 0)
+	{
+		return (a);
+	}
+}
