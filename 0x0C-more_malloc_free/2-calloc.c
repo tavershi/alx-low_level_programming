7321425644aeb0bb6f7d9e09c2af3d5958f8a38f
tavershi <gogobernard41@gmail.com>
2023-10-08 18:04:17 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,15 +7,31 @@
  * @n: number of times to copy b
  * Return: pointer to the memory area s
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ * Return: pointer to allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *output;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	output = calloc(nmemb, size);
-	if (output == NULL)
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
 		return (NULL);
-	else
-		return (output);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
 }
